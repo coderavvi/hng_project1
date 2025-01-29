63475b1cb502adab61bd7a38fd0124fa1a355f6a
coderavvi <iamtabson@gmail.com>
2025-01-29 15:11:00 +0100

    api GET route/endpoint is working as expected

---
+++ data.js
@@ -0,0 +1,9 @@
+const apiData = [
+  {
+    email: "iamtabson@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: "<https://github.com/coderavvi/hng_project1.git>",
+  },
+];
+
+export default apiData;
---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import apiData from "./data.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// / get endpoint for returning interns email, current datetime and github repo for the project
+app.get("/", (req, res) => {
+  res.json(apiData);
+});
+
+// connecting server to port 3000
+app.listen(PORT, () => {
+  console.log(`Server currently running on port ${PORT}`);
+});
