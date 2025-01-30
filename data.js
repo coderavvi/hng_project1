7379e85b9d98e900495ae54fe02dd1ee58b77004
Daniel Nehemiah Tabson <141719539+coderavvi@users.noreply.github.com>
2025-01-30 13:59:43 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const apiData =
   {
     email: "iamtabson@gmail.com",
     current_datetime: new Date().toISOString(),
-    github_url: "<https://github.com/coderavvi/hng_project1>",
+    github_url: "https://github.com/coderavvi/hng_project1",
   }
 
 export default apiData;
